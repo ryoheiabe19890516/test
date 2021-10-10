43b155af6e7d4b809a3a09d474aa73f6a76738be
阿部涼平 <aberyouhei@aberyouheinoMacBook-Pro.local>
2021-10-10 21:55:15 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and うんこ.
+          Edit <code>src/App.js</code> and イモーイェン・プーツ.
         </p>
         <a
           className="App-link"
